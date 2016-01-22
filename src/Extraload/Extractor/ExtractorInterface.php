dc9ab6095c39953380cf128d450f6c03803bd29c
umpirsky <umpirsky@gmail.com>
2016-01-22 20:46:46 +0100

    Initial CallbackTransformer implementation

--- src/Extraload/Extractor/ExtractorInterface.php
+++ src/Extraload/Extractor/ExtractorInterface.php
@@ -4,4 +4,5 @@ namespace Extraload\Extractor;
 
 interface ExtractorInterface extends \Iterator
 {
+    public function extract();
 }
---
+++ src/Extraload/Transformer/TransformerInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Extraload\Transformer;
+
+interface TransformerInterface
+{
+    public function transform($data);
+}
