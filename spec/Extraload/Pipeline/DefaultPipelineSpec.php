f64893de2540f31162bdf6e31969c91c8d05b4df
umpirsky <umpirsky@gmail.com>
2016-01-23 19:43:14 +0100

    Dispatch some events

--- spec/Extraload/Pipeline/DefaultPipelineSpec.php
+++ spec/Extraload/Pipeline/DefaultPipelineSpec.php
@@ -7,6 +7,8 @@ use Prophecy\Argument;
 use Extraload\Extractor\ExtractorInterface;
 use Extraload\Transformer\TransformerInterface;
 use Extraload\Loader\LoaderInterface;
+use Extraload\Events;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class DefaultPipelineSpec extends ObjectBehavior
 {
@@ -25,7 +27,11 @@ class DefaultPipelineSpec extends ObjectBehavior
         $this->shouldImplement('Extraload\Pipeline\PipelineInterface');
     }
 
-    function it_does_not_transform_nor_load_if_no_data_extracted(ExtractorInterface $extractor, TransformerInterface $transformer, LoaderInterface $loader)
+    function it_does_not_transform_nor_load_if_no_data_extracted(
+        ExtractorInterface $extractor,
+        TransformerInterface $transformer,
+        LoaderInterface $loader
+    )
     {
         $extractor->extract()->shouldBeCalled()->willReturn(null);
         $loader->flush()->shouldBeCalled();
@@ -33,12 +39,39 @@ class DefaultPipelineSpec extends ObjectBehavior
         $this->process();
     }
 
-    function it_processes_etl_sequentially(ExtractorInterface $extractor, TransformerInterface $transformer, LoaderInterface $loader)
+    function it_processes_etl_sequentially(
+        ExtractorInterface $extractor,
+        TransformerInterface $transformer,
+        LoaderInterface $loader
+    )
     {
         $extractor->extract()->shouldBeCalled()->willReturn(['a1', 'b1', 'c1'], null);
         $transformer->transform(['a1', 'b1', 'c1'])->shouldBeCalled()->willReturn(['c1', 'b1', 'a1']);
         $loader->load(['c1', 'b1', 'a1'])->shouldBeCalled();
         $loader->flush()->shouldBeCalled();
+
+        $this->process();
+    }
+
+    function it_dispatches_events_during_etl_processesing(
+        ExtractorInterface $extractor,
+        TransformerInterface $transformer,
+        LoaderInterface $loader,
+        EventDispatcherInterface $eventDispatcher
+    )
+    {
+        $this->beConstructedWith($extractor, $transformer, $loader, $eventDispatcher);
+
+        $extractor->extract()->shouldBeCalled()->willReturn(['a1', 'b1', 'c1'], null);
+        $transformer->transform(['a1', 'b1', 'c1'])->shouldBeCalled()->willReturn(['c1', 'b1', 'a1']);
+        $loader->load(['c1', 'b1', 'a1'])->shouldBeCalled();
+        $loader->flush()->shouldBeCalled();
+
+        $eventDispatcher->dispatch(Events::PRE_PROCESS, Argument::any())->shouldBeCalled();
+        $eventDispatcher->dispatch(Events::POST_PROCESS, Argument::any())->shouldBeCalled();
+        $eventDispatcher->dispatch(Events::EXTRACT, Argument::any())->shouldBeCalled();
+        $eventDispatcher->dispatch(Events::TRANSFORM, Argument::any())->shouldBeCalled();
+        $eventDispatcher->dispatch(Events::LOAD, Argument::any())->shouldBeCalled();
 
         $this->process();
     }
