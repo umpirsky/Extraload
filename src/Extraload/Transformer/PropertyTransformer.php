db30c20b679ebd00b65f79fcabf2dbb7dd4d20b3
umpirsky <umpirsky@gmail.com>
2016-01-29 23:41:49 +0100

    Extract data from Amazon

--- src/Extraload/Transformer/PropertyTransformer.php
+++ src/Extraload/Transformer/PropertyTransformer.php
@@ -2,7 +2,6 @@
 
 namespace Extraload\Transformer;
 
-use Extraload\Transformer\TransformerInterface;
 use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 
 class PropertyTransformer implements TransformerInterface
@@ -10,26 +9,31 @@ class PropertyTransformer implements TransformerInterface
     private $transformer;
     private $propertyAccessor;
     private $path;
+    private $transformNullValues;
 
     public function __construct(
         TransformerInterface $transformer,
         PropertyAccessorInterface $propertyAccessor,
-        $path
-    )
-    {
+        $path,
+        $transformNullValues = true
+    ) {
         $this->transformer = $transformer;
         $this->propertyAccessor = $propertyAccessor;
         $this->path = $path;
+        $this->transformNullValues = $transformNullValues;
     }
 
     public function transform($data)
     {
+        $value = $this->propertyAccessor->getValue($data, $this->path);
+        if (!$this->transformNullValues && null === $value) {
+            return $data;
+        }
+
         $this->propertyAccessor->setValue(
             $data,
             $this->path,
-            $this->transformer->transform(
-                $this->propertyAccessor->getValue($data, $this->path)
-            )
+            $this->transformer->transform($value)
         );
 
         return $data;
