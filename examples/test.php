1e47b71df1027657abe46e14ea4b81330f250e25
umpirsky <umpirsky@gmail.com>
2016-02-12 10:08:27 +0100

    Test script finally works

--- examples/test.php
+++ examples/test.php
@@ -3,10 +3,11 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\Yaml\Yaml;
 use Ko\ProcessManager;
 use Ko\Process;
 
-$config = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__.'/config.yml'));
+$config = Yaml::parse(file_get_contents(__DIR__.'/config.yml'));
 $broker = new Ko\AmqpBroker($config);
 $processManager = new ProcessManager();
 
@@ -14,19 +15,17 @@ $processManager->fork(function(Process $process) use ($broker) {
     $consumer = $broker->getConsumer('extractor');
     $message = 'Hello world ' . time();
     echo 'Receiving message `' . $message . '`' . PHP_EOL ;
+
     $consumer->consume(function (AMQPEnvelope $envelope) {
         echo 'Receive `' . $envelope->getBody() . PHP_EOL;
-
         return true;
     }, AMQP_AUTOACK);
 });
-
 $processManager->fork(function(Process $process) use ($broker) {
     $producer = $broker->getProducer('extractor');
     $message = 'Hello world ' . time();
     echo 'Sending message `' . $message . '`' . PHP_EOL ;
     $producer->publish($message);
-
 });
 
 $processManager->wait();
