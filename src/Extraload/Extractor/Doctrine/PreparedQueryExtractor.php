d418d819104d5a17dccbbf51e68ecd090caad1c3
programarivm <info@programarivm.com>
2018-04-22 13:00:04 +0100

    PreparedQueryExtractor row by row also (AbstractExtractor is created)

--- src/Extraload/Extractor/Doctrine/PreparedQueryExtractor.php
+++ src/Extraload/Extractor/Doctrine/PreparedQueryExtractor.php
@@ -3,68 +3,22 @@
 namespace Extraload\Extractor\Doctrine;
 
 use Doctrine\DBAL\Connection;
-use Extraload\Extractor\ExtractorInterface;
+use Extraload\Extractor\Doctrine\AbstractExtractor;
 
-class PreparedQueryExtractor implements ExtractorInterface
+class PreparedQueryExtractor extends AbstractExtractor
 {
-    private $position = 0;
-
-    private $data;
-
     public function __construct(Connection $conn, string $sql, array $values)
     {
-        $this->position = 0;
-
-        $stmt = $conn->prepare($sql);
-
+        $this->stmt = $conn->prepare($sql);
         foreach ($values as $value) {
-            $stmt->bindValue(
+            $this->stmt->bindValue(
                 $value['parameter'],
                 $value['value'],
                 $value['data_type'] ?? null
             );
         }
+        $this->stmt->execute();
 
-        $stmt->execute();
-
-        $this->data = $stmt->fetchAll();
-    }
-
-    public function extract()
-    {
-        if ($this->position >= count($this->data)) {
-            return;
-        }
-
-        $data = $this->current();
-
-        $this->next();
-
-        return $data;
-    }
-
-    public function current()
-    {
-        return $this->data[$this->position];
-    }
-
-    public function key()
-    {
-        return $this->position;
-    }
-
-    public function next()
-    {
-        $this->position += 1;
-    }
-
-    public function rewind()
-    {
         $this->position = 0;
-    }
-
-    public function valid()
-    {
-        return isset($this->data[$this->position]);
     }
 }
--- src/Extraload/Extractor/Doctrine/QueryExtractor.php
+++ src/Extraload/Extractor/Doctrine/QueryExtractor.php
@@ -3,60 +3,14 @@
 namespace Extraload\Extractor\Doctrine;
 
 use Doctrine\DBAL\Connection;
-use Extraload\Extractor\ExtractorInterface;
+use Extraload\Extractor\Doctrine\AbstractExtractor;
 
-class QueryExtractor implements ExtractorInterface
+class QueryExtractor extends AbstractExtractor
 {
-    private $stmt;
-
-    private $position;
-
-    private $data;
-
     public function __construct(Connection $conn, string $sql)
     {
         $this->stmt = $conn->query($sql);
         $this->position = 0;
         $this->data = [];
-    }
-
-    public function extract()
-    {
-        if (count($this->data) >= $this->stmt->rowCount()) {
-            return;
-        }
-
-        $this->data[$this->position] = $this->stmt->fetch();
-
-        $data = $this->current();
-
-        $this->next();
-
-        return $data;
-    }
-
-    public function current()
-    {
-        return $this->data[$this->position];
-    }
-
-    public function key()
-    {
-        return $this->position;
-    }
-
-    public function next()
-    {
-        $this->position += 1;
-    }
-
-    public function rewind()
-    {
-        $this->position = 0;
-    }
-
-    public function valid()
-    {
-        return isset($this->data[$this->position]);
     }
 }
