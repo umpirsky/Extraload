173476498690538f350cf919f714ea46949b6881
programarivm <info@programarivm.com>
2018-04-22 11:18:08 +0100

    the doctrine examples are bootstrapped

--- examples/doctrine/02_default_doctrine_prepared_query_console.php
+++ examples/doctrine/02_default_doctrine_prepared_query_console.php
@@ -1,25 +1,15 @@
 #!/usr/bin/env php
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../../vendor/autoload.php';
+require './mysql-bootstrap.php';
 
-use Doctrine\DBAL\Connection;
 use Extraload\Extractor\Doctrine\PreparedQueryExtractor;
 use Extraload\Loader\ConsoleLoader;
 use Extraload\Pipeline\DefaultPipeline;
 use Extraload\Transformer\NoopTransformer;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Output\ConsoleOutput;
-
-$config = new \Doctrine\DBAL\Configuration();
-$connectionParams = array(
-    'dbname' => 'extraload_fixtures',
-    'user' => 'extraload_fixtures',
-    'password' => 'password',
-    'host' => 'localhost',
-    'driver' => 'pdo_mysql',
-);
-$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
 
 $sql = "SELECT * FROM books WHERE author = :author";
 $values = [
